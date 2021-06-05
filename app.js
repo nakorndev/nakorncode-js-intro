30928089419fed039881705b32521cb36de188b7
Nakorn Sinpadung <nakorndev@gmail.com>
2021-06-05 11:51:42 +0700

    Add sample

---
+++ app.js
@@ -0,0 +1,9 @@
+// console.log('สวัสดี')
+// alert('สวัสดี')
+
+console.log('hi') // ;
+console.log('hello') // ;
+
+// Web browser (IE, Google Chrome, Firefox, Opera, Safari)
+
+document.getElementById('text').innerText = 'Hello world'
